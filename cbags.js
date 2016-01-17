89cb481a60274bcec104266cc05b5a4f8a6c33d1
mark <cococohen@gmail.com>
2016-01-17 10:13:31 -0800

    My First through webstorm

---
+++ cbags.js
@@ -0,0 +1,18 @@
+/**
+ * Created by mark on 1/16/16.
+ */
+
+function lengthFromDiameter(){
+    var getText = document.getElementById("circleWidth");
+    var disp = document.getElementById("dispArea");
+    disp.innerHTML = "Crochet for " + (getText.value  * 3.14).toPrecision(3) + " inches.";
+    getText.value = "";
+}
+
+function diameterFromLength(){
+    var getText = document.getElementById("bodyLength");
+    var disp = document.getElementById("dispArea");
+    disp.innerHTML = "Crochet circle for " + (getText.value / 3.14).toPrecision(3) + " inches.";
+    getText.value = "";
+}
+
